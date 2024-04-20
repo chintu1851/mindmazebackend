75273da20c35e73f7c0b22c23daa9000c7742339
chintu1851 <115821667+chintu1851@users.noreply.github.com>
2024-04-20 16:12:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 const mongoose = require('mongoose')
-const mongoString = "mongodb+srv://chintandaxeshpatel:Qwert1851@cluster0.r1ze08e.mongodb.net/players"
+// const mongoString = "mongodb+srv://chintandaxeshpatel:Qwert1851@cluster0.r1ze08e.mongodb.net/players"
 const app = express();
 
 var bodyParser = require('body-parser')
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
 }))
 
 
-mongoose.connect(mongoString);
+mongoose.connect("mongodb+srv://chintandaxeshpatel:Qwert1851@cluster0.r1ze08e.mongodb.net/players");
 const db = mongoose.connection
 
 db.once('connected', () => {
